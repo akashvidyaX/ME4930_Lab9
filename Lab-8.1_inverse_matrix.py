4c50faefdcf00a39a01693f521b16637b13d38bd
Saikishoreneppali <143101593+Saikishoreneppali@users.noreply.github.com>
2023-10-13 10:03:43 -0500

    Add files via upload

---
+++ Lab-8.1_inverse_matrix.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def inverse_rotation_matrix(R): #    The inverse of a rotation matrix is simply its transpose.
+
+
+    return R.T
+
+# function of R
+R = np.array([[0, -1, 0],
+              [1, 0, 0],
+              [0, 0, 1]])
+
+R_inverse = inverse_rotation_matrix(R)
+print("Inverse of R is:")
+print(R_inverse)
